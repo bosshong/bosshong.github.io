071c647abddab7066994cc204c3d3674f1aaaa1d
zhou <505524193@qq.com>
2017-01-04 13:13:07 +0800

    boot

--- personProject/JS/about.js
+++
@@ -1,9 +0,0 @@
-var oAbout1=document.getElementById('about1');
-var oAbout=document.getElementById('about');
-var oDelete2=document.getElementById('delete2');
-oAbout1.onclick=function(){
-    oAbout.style.display="block";
-};
-oDelete2.onclick=function(){
-    oAbout.style.display='none';
-};
--- personProject/JS/textarea.js
+++
@@ -1,9 +0,0 @@
-var oMes2=document.getElementById('mes2');
-var oMes=document.getElementById('mes');
-var oDelete1=document.getElementById('delete1');
-oMes2.onclick=function(){
-    oMes.style.display="block";
-};
-oDelete1.onclick=function(){
-    oMes.style.display='none';
-};
